4dfd65603a717be4c0fbe3b8a9f70fc5d69d68c5
manhaduy <haduyman@gmail.com>
2024-03-07 12:11:46 +0700

    Update the tour

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-r0010013",
-      "name": "R0010013",
+      "id": "0-bt-u",
+      "name": "Bắt Đầu",
       "levels": [
         {
           "tileSize": 256,
@@ -20,27 +20,44 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "faceSize": 1376,
+      "faceSize": 2752,
       "initialViewParameters": {
-        "yaw": -0.6028845009317259,
-        "pitch": -1.7763568394002505e-15,
-        "fov": 1.3926760049349705
+        "yaw": 0.04876964926145,
+        "pitch": 0.09057409757266122,
+        "fov": 1.3848081938437478
       },
       "linkHotspots": [
         {
-          "yaw": -0.6720661922549045,
-          "pitch": 0.4316448082395734,
-          "rotation": 12.566370614359176,
-          "target": "1-r0010014"
+          "yaw": 0.1539483316198016,
+          "pitch": 0.4885096647724314,
+          "rotation": 0,
+          "target": "1-1"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 1.2301317685439077,
+          "pitch": 0.12251678096215457,
+          "title": "Nơi ăn trưa lầu 9<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.1532896406337514,
+          "pitch": 0.03909290454956604,
+          "title": "Lối thoát hiểm<br>",
+          "text": "Text"
+        }
+      ]
     },
     {
-      "id": "1-r0010014",
-      "name": "R0010014",
+      "id": "1-1",
+      "name": "1",
       "levels": [
         {
           "tileSize": 256,
@@ -58,9 +75,62 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "faceSize": 1376,
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": 0.03302343294621757,
+        "pitch": 0.11177650700867758,
+        "fov": 1.3848081938437478
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.010961673201402178,
+          "pitch": 0.5254879357614755,
+          "rotation": 9.42477796076938,
+          "target": "0-bt-u"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.14076543610276993,
+          "pitch": 0.07031356908558806,
+          "title": "Lối thoát hiểm<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -68,93 +138,30 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.4450200798425321,
-          "pitch": 0.5663705095217253,
-          "rotation": 6.283185307179586,
-          "target": "2-r0010015"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-r0010015",
-      "name": "R0010015",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1376,
-      "initialViewParameters": {
-        "yaw": -1.9445984736820883,
-        "pitch": 0.0553326804255061,
-        "fov": 1.3926760049349705
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.4582067936070615,
-          "pitch": 0.547283222682271,
-          "rotation": 43.98229715025709,
-          "target": "3-r0010016"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-r0010016",
-      "name": "R0010016",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1376,
-      "initialViewParameters": {
-        "yaw": -0.7290286724873773,
-        "pitch": 0.0018495033266070493,
-        "fov": 1.3926760049349705
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.7302232230697498,
-          "pitch": 0.4385209036930373,
+          "yaw": -0.11496114638443267,
+          "pitch": 0.4210239466628334,
           "rotation": 0,
-          "target": "4-r0010017"
+          "target": "3-3"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.4870082290479729,
+          "pitch": 0.052480580082594486,
+          "title": "Phòng 2<br>",
+          "text": "<div>Nơi phòng làm việc của&nbsp;</div><div><br></div><div>Mẹ</div><div>Dì Ngọc</div><div>Dì Thanh</div><div>Mẫn<br></div>"
+        },
+        {
+          "yaw": -0.49587523839459813,
+          "pitch": 0.09722200716994323,
+          "title": "Bàn ăn trưa<br>",
+          "text": "Text"
+        }
+      ]
     },
     {
-      "id": "4-r0010017",
-      "name": "R0010017",
+      "id": "3-3",
+      "name": "3",
       "levels": [
         {
           "tileSize": 256,
@@ -172,23 +179,247 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "faceSize": 1376,
+      "faceSize": 2752,
       "initialViewParameters": {
-        "yaw": -0.9814120640060437,
-        "pitch": 0.012946523286249345,
-        "fov": 1.3926760049349705
+        "yaw": 1.672419579367208,
+        "pitch": 0.06838407763207321,
+        "fov": 1.3848081938437478
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 1.5279610861773758,
+          "pitch": 0.5066109669883883,
+          "rotation": 0,
+          "target": "4-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2472874338818967,
+          "pitch": 0.06949775566388183,
+          "title": "Thư viện<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3880242822419042,
+          "pitch": 0.6043605850506708,
+          "rotation": 1.5707963267948966,
+          "target": "5-45"
+        },
+        {
+          "yaw": -0.26635090936712835,
+          "pitch": 0.44355589680524155,
+          "rotation": 0,
+          "target": "6-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.718607437574911,
+          "pitch": 0.11975586283618966,
+          "title": "Bếp sinh hoạt<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.2579913493224133,
+          "pitch": 0.08972717052007795,
+          "title": "Lên sân thượng<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-45",
+      "name": "4.5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -3.0999732637345385,
+        "pitch": 0.03336887214080697,
+        "fov": 1.3848081938437478
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0759928628767934,
+          "pitch": 0.36107578601811774,
+          "rotation": 0,
+          "target": "4-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": 1.5368814119214491,
+        "pitch": 0.154759010186158,
+        "fov": 1.3848081938437478
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4630129718652984,
+          "pitch": 0.5126052017184861,
+          "rotation": 0,
+          "target": "7-56"
+        },
+        {
+          "yaw": 3.1054938080369165,
+          "pitch": 0.7473538771914434,
+          "rotation": 0,
+          "target": "4-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4833128411809495,
+          "pitch": 0.16961450660485156,
+          "title": "Đường lên sân thượng<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.127798099940134,
+          "pitch": 0.1548864692218359,
+          "title": "Vào lại nhà bếp<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-56",
+      "name": "5.6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "yaw": -1.4735666894559483,
+        "pitch": 0.09154090071403154,
+        "fov": 1.3848081938437478
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5480405720787989,
+          "pitch": 0.5339384532220564,
+          "rotation": 0,
+          "target": "6-5"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Lau 9 Cong ty Lac Viet",
   "settings": {
-    "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
